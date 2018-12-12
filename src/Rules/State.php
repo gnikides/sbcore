15fe506e5c0524388d84d0b4874b23251b524b19
gnikides <gnikides@yahoo.com>
2018-12-13 00:46:01 +0100

    Add rules

---
+++ src/Rules/State.php
@@ -0,0 +1,33 @@
+<?php namespace Core\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class State implements Rule
+{
+    public $country_code;
+    public $is_state_required;
+
+    public function __construct(string $country_code, bool $is_state_required = false)
+    {
+        $this->country_code = isset($country_code) ? strtoupper($country_code) : '';
+        $this->is_state_required = $is_state_required;
+    }
+
+    public function passes($attribute, $value)
+    {
+        return $this->validate(preg_replace('/\s+/', '', $value));
+    }
+
+    public function message()
+    {
+        return trans('The state is invalid.');
+    }
+
+    public function validate($state)
+    {        
+        if ($this->is_state_required && !$state) {  
+            return false;
+        }
+        return true;
+    }
+}
