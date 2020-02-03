8df9a0b776e19073ca82db7e97501f9ec47f8560
gnikides <gnikides@yahoo.com>
2020-02-04 00:29:37 +0100

    Update

---
+++ src/ProductSearch/IndexResource.php
@@ -0,0 +1,72 @@
+<?php namespace Core\ProductSearch;
+
+use Core\Support\Resource;
+use App\Modules\Currency;
+use App\Http\Resources\ProductSlimResource;
+
+class IndexResource extends Resource
+{ 
+    protected $expandable = [
+        'store',
+        'country'
+    ];
+
+    public function toArray($request)
+    {   
+        $reference = $this->reference;
+        $format = $this->format;
+        $version = $this->version;
+        $store = $this->store;
+        $price = $this->price;
+        $currency = new Currency($price->currency_code);
+        
+        //sb($version);
+
+        $name = '';
+        //sb($format->props);
+        if ($format && isset($format->props)) {
+            $name = isset($format->props['default']['name']['value']) ? $format->props['default']['name']['value'] : '';
+        } 
+        // sb($name);
+        // exit();
+        $s = $this->filter([
+            'product_id'            => $this->id,
+            'name'                  => $name,
+            'manufacturer'          => $reference->manufacturer,
+            'brand'                 => $reference->brand,
+            'creator'               => $reference->creator,
+            'sku'                   => $reference->sku,
+            'publisher_reference'   => $reference->publisher_reference,
+            'product_group_id'      => $reference->product_group_id,
+            // 'category_id'           => $reference->category_id,
+            // 'category_name'         => $reference->category->name,
+            'updated_at'            => isset($version->updated_at) ? (string) $version->updated_at : now(),
+            'retail_price'          => $currency->fromCents($price->retail_price),
+            'wholesale_price'       => $currency->fromCents($price->wholesale_price),
+            'store_id'              => $store->id,
+            'store_name'            => $store->name,
+            'store_handle'          => $store->handle,
+            'country_code'          => $store->country_code,
+            // 'country'               => $site->country->name,
+            'average_rating'        => $reference->average_rating,
+            'number_ratings'        => $reference->number_ratings,
+            // 'categories'            => collect($this->categories)->transform(function ($item) {
+            //                                 return $item->id;
+            //                         }),
+
+            //  @todo
+            //  put a lot of text into search
+            //'descriptions'          => json_encode($reference->descriptions),
+            //'features'              => json_encode($reference->features),
+            // 'details'               => [
+            //                             'Manufacturer',
+            //                             'Author',
+            //                             'Brand'
+            //                         ],
+            'content'               => json_encode(new ProductSlimResource($object))
+        ]);
+        // sb($s);
+        // exit();
+        return $s;
+    }
+}
---
+++ src/Support/Resource.php
@@ -0,0 +1,59 @@
+<?php namespace Core\Support;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Resource extends JsonResource
+{
+    protected $expandable = [];
+    const ACTIVE = 'active';
+    const INACTIVE = 'inactive';
+
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+
+    public function expands($node, $value, $relation = '')
+    {
+        if (!$relation) {
+            $relation = $node;
+        }
+        return $this->when($this->isExpandable($node) && is_object($this->{$relation}), $value);
+    }
+
+    public function notExpands($node, $value, $relation = '')
+    {
+        if (!$relation) {
+            $relation = $node;
+        }        
+        return $this->when(!$this->isExpandable($node) || !is_object($this->{$relation}), $value);
+    }
+    
+    public function isExpandable($endpoint)
+    {
+        return in_array($endpoint, $this->expandable); 
+    } 
+
+    public function setExpandable($endpoints = [])
+    {
+        $this->expandable = $endpoints;
+        return $this;
+    }
+
+    public function noExpands()
+    {
+        $this->expandable = [];
+        return $this;
+    }
+
+    public function setData($key, $value)
+    { 
+        $this->data[$key] = $value;
+        return $this;
+    }
+
+    public function getData($key)
+    {
+        return array_get($this->data, $key, null);
+    }    
+}
