0e2da9245625b0482b3fb3ce2c56eda71504c52f
gnikides <gnikides@users.noreply.github.com>
2025-04-01 01:34:18 +0200

    Create NotUrl.php

---
+++ src/Rules/NotUrl.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class NotUrl implements Rule
+{
+    public function passes($attribute, $value)
+    {
+        // Regex to check if the text contains a URL
+        return !preg_match('/https?:\/\/[^\s]+/', $value);
+    }
+
+    public function message()
+    {
+        return 'The text cannot contain a URL';
+    }
+}
